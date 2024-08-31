7f45db52fa2f347a3fbbddec5e140c04b6deb1cb
Kamran <kamranmuazzam@gmail.com>
2024-08-31 13:04:58 +0600

    removing dstore

--- g.sh
+++ g.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+
+# Check if the commit message is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <commit message>"
+    exit 1
+fi
+
+# Run Git commands
 git pull
 git add --all
-git commit -m "update"
+git commit -m "$1"
 git push -u origin main
+
